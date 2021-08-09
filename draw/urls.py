056e34fde799136343616f60e7a33e5483ad5c64
Noah Schwartz <n0112358@berkeley.edu>
2021-08-08 20:48:51 -0700

    created a file for every page

--- draw/urls.py
+++ draw/urls.py
@@ -5,5 +5,14 @@ from . import views
 
 urlpatterns = [
     path('welcome', views.welcome, name='welcome'),
+    path('Checklist', views.Checklist, name='Checklist'),
+    path('Companies', views.Companies, name='Companies'),
+    path('CompaniesEnlarge', views.CompaniesEnlarge, name='CompaniesEnlarge'),
+    path('CompaniesSearch', views.CompaniesSearch, name='CompaniesSearch'),
+    path('DailyTasks', views.DailyTasks, name='DailyTasks'),
+    path('Leaderboard', views.Leaderboard, name='Leaderboard'),
+    path('LogIn', views.LogIn, name='LogIn'),
+    path('MonthlyTasks', views.MonthlyTasks, name='MonthlyTasks'),
+    path('WeeklyTasks', views.WeeklyTasks, name='WeeklyTasks'),
     path('<str:room_name>/', views.room, name='room'),
 ]
--- draw/views.py
+++ draw/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request, 'draw/welcome.html')
+def Checklist(request):
+    return render(request, 'draw/Checklist.html')
+def Companies(request):
+    return render(request, 'draw/Companies.html')
+def CompaniesEnlarge(request):
+    return render(request, 'draw/CompaniesEnlarge.html')
+def CompaniesSearch(request):
+    return render(request, 'draw/CompaniesSearch.html')
+def DailyTasks(request):
+    return render(request, 'draw/DailyTasks.html')
+def Leaderboard(request):
+    return render(request, 'draw/Leaderboard.html')
+def LogIn(request):
+    return render(request, 'draw/LogIn.html')
+def MonthlyTasks(request):
+    return render(request, 'draw/MonthlyTasks.html')
+def WeeklyTasks(request):
+    return render(request, 'draw/WeeklyTasks.html')
 
 def room(request, room_name):
     return render(request, 'draw/room.html', {
